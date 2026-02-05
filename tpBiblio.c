73ceb4ebcdc593b1ad0411e9bf25c8d3d1b30dd9
Iner <inerdenormandie@gmail.com>
2026-02-05 09:53:18 +0100

    ajout sauvegarde

--- tpBiblio.c
+++ tpBiblio.c
@@ -70,10 +70,37 @@ switch(chx)
 }while(chx!=0);
 
 
-
+sauvegarde(&B);
 
 
 
 return 0;
+
+}
+
+void sauvegarde(T_Bibliotheque *ptrB)
+{
+FILE *fic=NULL; //le type FILE
+int i;
+fic=fopen("baseLivres","w"); // w = le mode = write avec ECRASEMENT
+//fopen renvoie NULL si probleme (disque plein, disque non accessible ...
+if (fic!=NULL)
+	{
+	for(i=0;i<ptrB->nbLivres;i++)
+		{
+//fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
+    fwrite(  &(ptrB->etagere[i])        ,sizeof(T_livre),1,fic);
+
+		}
+	fclose(fic);
+	puts("SAUVEGARDE REUSSIE ..............");
+
+
+
+	}
+	else puts("ECHEC DE SAUVEGARDE  !!!!!  ");
+
+
+
 
 }
